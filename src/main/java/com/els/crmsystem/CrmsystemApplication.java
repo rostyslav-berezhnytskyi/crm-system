ef02859ff1fadf748c4b3a2682444300b7eeea46
Rostyslav <rostik2510@gmail.com>
2026-02-14 16:40:05 +0200

    Initial commit

---
+++ src/main/java/com/els/crmsystem/CrmsystemApplication.java
@@ -0,0 +1,13 @@
+package com.els.crmsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmsystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrmsystemApplication.class, args);
+    }
+
+}
