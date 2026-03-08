5320fb322694b00e467e14e5ac49dc6a30ac9ce1
Rostyslav <rostik2510@gmail.com>
2026-03-09 00:14:02 +0200

    Change sql shema of flyway. First relese version!

--- src/main/resources/db/migration/V6__project_additional_contacts.sql
+++
@@ -1,6 +0,0 @@
--- Create a junction table for Many-to-Many relationship between Projects and Contacts
-CREATE TABLE project_additional_contacts (
-                                             project_id BIGINT REFERENCES projects(id) ON DELETE CASCADE,
-                                             contact_id BIGINT REFERENCES contacts(id) ON DELETE CASCADE,
-                                             PRIMARY KEY (project_id, contact_id)
-);
