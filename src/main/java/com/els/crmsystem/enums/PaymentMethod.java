a2606ef47789f519d1f49b3b8b2be3b374a3ae3a
Rostyslav <rostik2510@gmail.com>
2026-05-30 10:05:57 +0300

    Add payment method FOP

--- src/main/java/com/els/crmsystem/enums/PaymentMethod.java
+++ src/main/java/com/els/crmsystem/enums/PaymentMethod.java
@@ -9,7 +9,8 @@ public enum PaymentMethod {
     CASH("Cash", "Готівка"),
     CARD_PERSONAL("Personal Card", "Особиста картка"),
     CARD_COMPANY("Company Card", "Корпоративна картка"),
-    IBAN("Bank Transfer", "Безготівковий розрахунок (IBAN)");
+    IBAN("Bank Transfer", "Безготівковий розрахунок (IBAN)"),
+    FOP("ФОП", "ФОП Бережницька Т.М.");
 
     private final String englishName;
     private final String ukrainianName;
